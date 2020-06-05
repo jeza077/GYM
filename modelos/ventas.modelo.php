14c200e0afd63d473cebb5f6b7a769f042c36150
Jeza077 <jesus.zuniga077@gmail.com>
2020-06-05 16:24:36 -0600

    Agregamos el dashboard/inicio

--- modelos/ventas.modelo.php
+++ modelos/ventas.modelo.php
@@ -158,7 +158,21 @@ class ModeloVentas{
 
 		}
 
+	}    
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+    /*=============================================
+            SUMAR EL TOTAL DE VENTAS
+	=============================================*/
+	static public function mdlSumaTotalVentas($tabla){
+
+		$stmt = Conexion::conectar()->prepare("SELECT SUM(neto) as total FROM $tabla");
+		$stmt -> execute();
+		return $stmt -> fetch();
+
+		$stmt -> close();
+		$stmt = null;
 		
 	}    
-	
+
 }
