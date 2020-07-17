2d1be4358b53acabc639d2bebafcd4200e2bb8dc
Jeza077 <jesus.zuniga077@gmail.com>
2020-07-16 20:35:53 -0600

    CAMBIO NOMBRE BD

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos-usuarios",
+		$link = new PDO("mysql:host=localhost;dbname=pos",
 			            "root",
 			            "");
 
