45d656b81e60f6d22b04427aa3b18306fa3dd68e
Jeza077 <jesus.zuniga077@gmail.com>
2020-07-20 20:47:48 -0600

    Modulo recupepar password

---
+++ vistas/modulos/recuperar-password.php
@@ -0,0 +1,40 @@
+
+<div class="login-box">
+  
+  <div class="login-logo">
+
+    <img src="vistas/img/plantilla/logo-blanco-bloque.png" class="img-responsive" style="padding:30px 100px 0px 100px">
+
+  </div>
+
+  <div class="login-box-body">
+
+    <p class="login-box-msg">Recuperar contraseña</p>
+
+    <form method="post">
+    
+
+      <div class="form-group has-feedback">
+
+        <input type="text" class="form-control" placeholder="Usuario" name="ingUsuario" required>
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+
+      </div>
+
+
+      <div class="row">
+       
+        <div class="col-xs-12">
+
+          <button type="submit" class="btn btn-naranja btn-block btn-flat">Enviar</button>
+        
+        </div>
+
+      </div>
+
+
+    </form>
+    
+  </div>
+
+</div>
